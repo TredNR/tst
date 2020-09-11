6b75cea3f2c3428cca44df17c4491662f70eb580
Konstantin Ivanov <69236482+TredNR@users.noreply.github.com>
2020-09-11 15:48:37 +0400

    Update MongoTest.py

--- test_streamlit/MongoTest.py
+++ test_streamlit/MongoTest.py
@@ -4,60 +4,64 @@ import pandas as pd
 
 st.title("Test with MongoDB")
 
-@st.cache(hash_funcs={MongoClient: id})
-def get_client():
-    return MongoClient("mongodb://127.0.0.1/admin")
+#@st.cache(hash_funcs={MongoClient: id})
+#def get_client():
+#    return MongoClient("mongodb://127.0.0.1/admin")
+
+enter = st.text_input("Address",'mongodb://localhost:27017')
+if st.button("Enter", key=1):
+    result = enter.title()
+    st.success(result)
+
+client = MongoClient(enter)
 
 # ------------- Настройка выбора коллекции --------------
 
-client = get_client()
+#client = get_client()
 
-db = client.CovidModel3
-a = db.list_collection_names()
+db = client.list_database_names()
+c = st.write(db)
+select1 = st.selectbox("Select a database", db)
+selected_database = client[select1]
+
+a = selected_database.list_collection_names()
 b = st.write(a)
-
 select = st.selectbox("Select a collection", a)
-selected_filename = db[select]
+selected_filename = selected_database[select]
 
 collection_select = pd.DataFrame(list(selected_filename.find()))
 
 # ------------------ Превью коллекции ---------------------
 
-if st.button("PREVIEW", key=1):
+if st.checkbox("Preview collection", key=1):
     number = st.number_input("Number of Rows to View",1, 200)
     st.dataframe(collection_select.head(number))
 
 # --------------- Показать всю коллекцию -----------------
 
-if st.checkbox("All collection"):
+if st.checkbox("Show all collection", key=2):
     view = st.write(collection_select)
 
 # ---------------------- Тип данных ------------------------
 
-if st.checkbox("Data Types"):
+if st.checkbox("Data Types in the collection", key=3):
     st.write(collection_select.dtypes)
 
 # --------- Общая информация о кол-ве стобцов/строк ---------
 
-if st.checkbox("Shape of Dataset"):
-    st.write(collection_select.shape)
-    data_dim = st.radio("Show Dimension By ",("Rows", "Columns"))
-    if data_dim == 'Rows':
-        st.text("Number of Rows")
-        st.write(collection_select.shape[0])
-    elif data_dim == 'Columns':
-        st.text("Number of Columns")
-        st.write(collection_select.shape[1])
-    else:
-        st.write(collection_select.shape)
+if st.checkbox("Shape of Dataset", key=4):
+    st.text("Number of Rows")
+    st.write(collection_select.shape[0])
+    st.text("Number of Columns")
+    st.write(collection_select.shape[1])
 
 # --------------- Нарисуем какие-нибудь графики ---------------
 
-if st.checkbox("Plot"):
-    st.write(collection_select.describe().T)
-    st.subheader("Data Visualization")
-    all_columns_names = collection_select.columns.tolist()
+if st.checkbox("Plot with select columns", key=5):
+    st.text("Works extremely unstable, needs to be improved")
     type_of_plot = st.selectbox("Select Type of Plot", ["Area", "Bar", "Line", "Hist", "Box", "Kde"])
+
+    all_columns_names = collection_select.columns.tolist()
     selected_columns_names = st.multiselect("Select Columns To Plot", all_columns_names)
 
     if st.button("Generate Plot"):
@@ -79,4 +83,6 @@ if st.checkbox("Plot"):
         cust_plot = collection_select[selected_columns_names].plot(kind=type_of_plot)
         st.write(cust_plot)
         st.pyplot()
+
+
 
